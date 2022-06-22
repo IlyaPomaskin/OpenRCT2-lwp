36e2f3dc17e4c9a609ceff5df8c59a603f368cc4
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-06-22 14:23:32 +0200

    add SDLMain logging

--- src/openrct2-android/app/src/main/java/org/libsdl/app/SDLMain.java
+++ src/openrct2-android/app/src/main/java/org/libsdl/app/SDLMain.java
@@ -3,10 +3,15 @@ package org.libsdl.app;
 import android.util.Log;
 
 public class SDLMain implements Runnable {
+    public String TAG = "SDLMain";
+
     @Override
     public void run() {
+        TAG = TAG + Math.round((Math.random() * 10));
+        Log.v(TAG, "SDL thread start");
+
         SDLActivity.nativeInit(SDLActivity.mSingleton.getArguments());
 
-        Log.v("SDL", "SDL thread terminated");
+        Log.v(TAG, "SDL thread terminated");
     }
 }
